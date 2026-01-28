4f4102a30ee1abc5c80314958108b1bdfe0428b2
Aniket Agrawal <ankt7987@gmail.com>
2026-01-28 22:50:33 +0530

    .

--- product/src/services/imagekit.service.js
+++ product/src/services/imagekit.service.js
@@ -1,8 +1,6 @@
 const ImageKit = require('imagekit');
 const { v4: uuidv4 } = require("uuid")
 
-
-
 const imagekit = new ImageKit({
     publicKey: process.env.IMAGEKIT_PUBLIC_KEY || 'test_public',
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY || 'test_private',
