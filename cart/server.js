14035f0ffe44ea5e249a8899f8d8a08b2dc638f9
Aniket Agrawal <ankt7987@gmail.com>
2026-02-19 22:13:43 +0530

    Cart-Service

---
+++ cart/server.js
@@ -0,0 +1,10 @@
+reuire('dotenv').config();
+const app = require('./src/app');
+const connectDB = require('./src/db/db');
+
+connectDB();
+
+
+app.listen(3002, () => {
+    console.log('Cart service running on port 3002');
+});
---
+++ cart/src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cartRoutes = require('./routes/cart.routes');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: "Cart service is running"
+    });
+})
+
+
+app.use('/api/cart', cartRoutes);
+
+
+
+module.exports = app;
