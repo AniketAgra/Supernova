a6aaf9b708008d73fafe09248e344bc8f8478be4
Aniket Agrawal <ankt7987@gmail.com>
2026-04-29 23:15:08 +0530

    Paymeny_service1

---
+++ payment/server.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const connectDB = require('./src/db/db');
+const app = require('./src/app');
+
+
+connectDB();
+
+
+app.listen(3004, () => {
+  console.log('Payment service is running on port 3004');
+});
