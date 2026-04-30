8faaa3e4b13ea1a3a783cdb4dce6823dc12f99c6
Aniket Agrawal <ankt7987@gmail.com>
2026-04-30 15:38:29 +0530

    ai-buddy

---
+++ ai-buddy/src/sockets/socket.server.js
@@ -0,0 +1,32 @@
+const {Server} = require('socket.io');
+const jwt = require('jsonwebtoken');
+const cookie = require('cookie');
+
+async function initSocketServer(httpServer) {
+  const io = new Server(httpServer, {});
+
+  io.use((socket, next) => {
+    const cookies = socket.handshake.headers?.cookie;
+
+    const {token} = cookie.parse(cookies || '');
+
+    if (!token) {
+      return next(new Error('Authentication error: No token provided'));
+    }
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        socket.user = decoded;
+        next();
+    } catch (error) {
+        return next(new Error('Authentication error: Invalid token'));
+    }
+  });
+
+  io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+  });
+
+}
+
+module.exports = {initSocketServer};
