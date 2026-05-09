08a960e0e56b2560f21ece71381bdaebf7732858
Aniket Agrawal <ankt7987@gmail.com>
2026-05-10 02:27:31 +0530

    Seller Dashboard setup

---
+++ seller-dashboard/src/models/product.model.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+
+const productSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+    },
+    price: {
+        amount: {
+            type: Number,
+            required: true
+        },
+        currency: {
+            type: String,
+            enum: [ 'USD', 'INR' ],
+            default: 'INR'
+        }
+    },
+    seller: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+    },
+    images: [
+        {
+            url: String,
+            thumbnail: String,
+            id: String
+        }
+    ],
+    stock: {
+        type:Number,
+        default: 0
+    },
+})
+
+// indexes for efficient querying, it basically creates a text index on title and description for text search, and an index on seller for filtering by seller
+//whatever input we give in a parameter, it will search for the closest match in the title and description fields of the products collection. This allows for more flexible searching, as it can find products even if the search term is not an exact match.
+productSchema.index({ title: 'text', description: 'text' }); // For text search,
+
+
+const productModel = mongoose.model('product', productSchema);
+
+module.exports = productModel;  
---
+++ seller-dashboard/src/models/user.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const addressSchema = new mongoose.Schema({
+        street: String,
+        city: String,
+        state: String,
+        zip: String,
+        country: String,
+        isDefault: { type: Boolean, default: false }
+});
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, select: false,  required: true }, // select is used to exclude password field by default - whenever we query user data like by using find(),findOne(),etc. It will not return password field unless we explicitly ask for it
+    fullName: {
+        firstName: { type: String , required: true},
+        lastName : { type: String, required: true}
+    },
+    role: {
+        type: String,
+        enum: ['user', 'seller'],
+        default: 'user'
+    },
+    address: [addressSchema]
+}, { timestamps: true });
+
+const userModel = mongoose.model('user', userSchema);
+
+module.exports = userModel;
